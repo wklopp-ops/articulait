5660586ac6e1ef75bf2d85eece47f8abff5e50b1
wklopp-ops <wklopp@gmail.com>
2025-11-21 15:21:39 -0800

    Create Logo.tsx

---
+++ app/app/app/components/components/Logo.tsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+  return (
+    <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M8 8H40V40H8V8Z" fill="currentColor" opacity="0.1"/>
+      <text x="12" y="32" fontFamily="Obviously, sans-serif" fontSize="28" fontWeight="900" fill="currentColor">AI</text>
+    </svg>
+  )
+}
